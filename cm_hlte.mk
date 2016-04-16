225f2cab5fdc79c6151c287eca5b14f627cdedfe
James Christopher Adduono <jc@adduono.com>
2016-04-16 18:06:25 -0400

    CyanogenModitize

--- cm_hlte.mk
+++ cm_hlte.mk
@@ -4,10 +4,10 @@ PRODUCT_RELEASE_NAME := hlte
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := omni_hlte
+PRODUCT_NAME := cm_hlte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo omni_hlte-eng
-add_lunch_combo omni_hlte-userdebug
+add_lunch_combo cm_hlte-eng
+add_lunch_combo cm_hlte-userdebug
